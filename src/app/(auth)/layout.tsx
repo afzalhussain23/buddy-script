1f952512b5fa8e8b65cf4da0d508396bec9b939d
Afzal Hussain <afzalhussain@iut-dhaka.edu>
2026-06-15 16:31:53 +0600

    Set Buddy Script app metadata

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,4 @@
-import type { Metadata } from "next";
 import Script from "next/script";
-
-export const metadata: Metadata = {
-  title: "Buddy Script",
-};
 
 export default function AuthLayout({
   children,
